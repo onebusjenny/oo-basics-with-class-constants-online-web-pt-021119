a26ba3f50015f820cd313a5330ef0d49285dd8a2
Jenny Chu <jchudesign0930@gmail.com>
2019-03-14 18:48:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,9 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand 
+
+  BRANDS = ["Uggs","Rainbow"]
+  
 
   def initialize(brand)
     @brand = brand
@@ -11,4 +14,13 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand= (brand)
+    @brand = brand
+    BRANDS << brand 
+
+   end
+   
+  
+  
+  
 end
